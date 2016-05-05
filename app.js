06362bb9c270a42a00790dcf22f52815c832a9b3
JuanHenriquez <juanhenriquezzavarse18@gmail.com>
2016-05-05 00:11:26 -0430

    FEATURES:
    1.- Integrate the app with Mongodb.
    2.- Setup Mongodb.
    3.- Modify the routes and interact with the db.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // Requier all the dependences.
 var express    = require('express'),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    mongoose   = require('mongoose');
 
 // Set the app
 var app = express();
@@ -12,35 +13,72 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// =============
-// ROUTES
-// =============
+// ===============
+// DB Setup ======
+// ===============
 
-var campgrounds = [
-    { name: 'Salmo Creek', image: "http://www.photosforclass.com/download/7842069486" },
-    { name: 'Granite Hill', image: "http://www.photosforclass.com/download/10131087094" },
-    { name: 'Mountain Goat', image: "http://www.photosforclass.com/download/4661960920" }
-];
+// Connect to the mongodb database.
+mongoose.connect('mongodb://localhost/yelp_camp');
+
+// SCHEMA SETUP:
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+var Campground = mongoose.model('Campground', campgroundSchema);
+
+// Create a new campground and add to DB.
+// Campground.create(
+//     {
+//         name: 'Granite Hill',
+//         image: "http://www.photosforclass.com/download/10131087094"
+//     }, function(err, camp) {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             console.log('NEW Campground added!');
+//             console.log(camp);
+//         }
+//     }
+// );
+
+// =============
+// ROUTES ======
+// =============
 
 app.get('/', function(rq, res){
     res.render('home');
 });
 
 app.get('/campgrounds', function(req, res){
-
-    res.render('campgrounds', { campgrounds: campgrounds });
-
+    // get all campgrounds from DB.
+    Campground.find({}, function(err, campgrounds){
+        if (err) {
+            console.log('ERROR: ' + err);
+        } else {
+            // If no errors occurs then render the campgrounds
+            // templae with the data retrive from DB.
+            res.render('campgrounds', { campgrounds: campgrounds });
+        }
+    });
 } );
 
 app.post('/campgrounds', function(req, res){
     // Get the data from the Request Body.
     var name = req.body.name;
     var img = req.body.img;
+    var data = { name: name, image: img };
 
-    // Push new data to the campgrouns array.
-    campgrounds.push({ name: name, image: img });
-    // Redirect back to the campgrounds page.
-    res.redirect('/campgrounds');
+    Campground.create( data, function(err, data){
+        if (err) {
+            console.log('ERROR: ' + err);
+        } else {
+            console.log('Added new campground: ' + data.name);
+            // Redirect back to the campgrounds page.
+            res.redirect('/campgrounds');
+        }
+    });
 });
 
 app.get('/campgrounds/new', function(req, res){
