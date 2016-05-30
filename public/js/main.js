a8c12cc13841192fb266643beb9d474708d07429
Juan Henriquez <juanhenriquezzavarse18@gmail.com>
2016-05-30 03:30:03 -0430

    Add dropdown menu functionality plugin.

--- public/js/main.js
+++ public/js/main.js
@@ -2,5 +2,13 @@ $(document).ready(function(){
 
     $(".button-collapse").sideNav();
     $('.parallax').parallax();
-
+    $('.dropdown-button').dropdown({
+        inDuration: 300,
+        outDuration: 225,
+        constrain_width: true, // Does not change width of dropdown to that of the activator
+        hover: false, // Activate on hover
+        gutter: 0, // Spacing from edge
+        belowOrigin: true, // Displays dropdown below the button
+        }
+    );
 });
