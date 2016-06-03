a55d38ba8a35c70b237c244bc27247e920706336
Juan Henriquez <juanhenriquezzavarse18@gmail.com>
2016-06-03 01:56:53 -0430

    Refactor root and user authentication routes.

---
+++ routes/index.js
@@ -0,0 +1,77 @@
+var express  = require('express');
+var router   = express.Router();
+var multer   = require('multer');
+var crypto   = require('crypto');
+var mime     = require('mime');
+var User     = require('../models/user');
+var passport = require('passport');
+
+// Config multer for upload pictures.
+var uploading = multer({
+    limits: { fileSize: 10000000, files: 1},
+    storage: multer.diskStorage({
+        destination: function(req, file, cb) {
+            cb(null, './public/img/user_pictures');
+        },
+        filename: function (req, file, cb) {
+            crypto.pseudoRandomBytes(16, function (err, raw) {
+                cb(null, raw.toString('hex') + Date.now() + '.' + mime.extension(file.mimetype));
+            });
+        }
+    })
+});
+
+router.get('/', function(req, res){
+    res.render('home');
+});
+
+
+// Show register template.
+router.get('/register', function(req, res) {
+    res.render('register');
+});
+
+// New - Register a user
+router.post('/register', uploading.single('picture'), function(req, res, next) {
+    var newUser = new User({
+        username: req.body.username,
+        name: req.body.name,
+        age: req.body.age,
+        email: req.body.email,
+        picture: req.file.filename
+    });
+    User.register(newUser, req.body.password, function(err, user){
+        if (err) {
+            console.log('Error: ' + err);
+            return res.render('register');
+        }
+        passport.authenticate('local')(req, res, function() {
+            res.redirect('/campgrounds');
+        });
+
+    });
+});
+
+router.get('/login', function(req, res) {
+    res.render('login');
+});
+
+router.post('/login', passport.authenticate('local', {
+    'successRedirect': "/campgrounds",
+    'failureRedirect': "/login"
+}));
+
+router.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/campgrounds  ');
+});
+
+// Middleware.
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect("/login");
+}
+
+module.exports = router;
